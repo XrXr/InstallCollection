05c2ca88245c6b18d16e3162556fadf87cfc15c8
XrXr <xrsquared@gmail.com>
2014-04-10 16:59:18 -0400

    Button makes populated side bar, removed page-mod
    Todo:
     - UI completion
     - Locking zero add-on page with state()
     - Forbid repopulation of side bar after installation starts

--- data/side_bar/side_bar.js
+++ data/side_bar/side_bar.js
@@ -1,10 +1,13 @@
+addon.port.emit("ping");
+
 //name space
 var install_collection = {};
 
 //modules
 install_collection.Add_on = function(data){
-    this.name =  m.prop(data.name);
-    this.url =  m.prop(data.url);
+    this.name = m.prop(data.name);
+    this.url = m.prop(data.url);
+    this.progress = m.prop(data.progress);
 };
 
 install_collection.Add_on_list = Array;
@@ -14,21 +17,14 @@ install_collection.view = function(ctrl){
     return  ctrl.list.map(function (add_on, index){
         return m('div', {style: {margin:'0 5% 0 5%'}},[
                     m('div', {class: 'progress'},[
-                        m('div', {class: 'progress-bar', role: 'progressbar', style: {width: '40%'}}, add_on.name())
+                        m('div', {class: 'progress-bar',
+                            role: 'progressbar',
+                            style: {width: '40%'}}, [
+                                m('span', {style: {color: 'black'}}, add_on.name())
+                            ])
                     ])
             ]);
     });
-            
-    // return m('html',[
-    //     m('body',[
-    //         m('div', {style: {margin:'0 0 5% 5%'}},[
-    //             m('div', {class: 'progress'},[
-    //                 m('div', {class: 'progress-bar', role: 'progressbar', style: {width: '40%'}})
-    //             ])
-    //         ])
-    //     ])
-    //    ]);
-
 };
 
 //controller
@@ -36,20 +32,28 @@ install_collection.controller = function(){
     this.list = new install_collection.Add_on_list();
     this.name = m.prop("");
     this.url = m.prop("");
-
     this.add = function(name, url){
         if (name()) {
-            this.list.push(new install_collection.Add_on({name: name(), url: url()}));
+            this.list.push(new install_collection.Add_on(
+                {name: name(), url: url(), progress: 0}));
             this.name("");
             this.url("");
         }
     };
 };
 
-
 var ctrl = new install_collection.controller();
 ctrl.name("Write code");
 ctrl.url("Good luck!");
-ctrl.add(ctrl.name,ctrl.url);
+ctrl.add(ctrl.name, ctrl.url);
 console.log(ctrl.list.length); //1
 m.render(document.body, install_collection.view(ctrl));
+
+addon.port.on("add-ons", function(add_ons) {
+    for (var i = 0; i < add_ons.names.length; i++) {
+        ctrl.name(add_ons.names[i]); 
+        ctrl.url(add_ons.urls[i]); 
+        ctrl.add(ctrl.name, ctrl.url);
+    }
+    m.render(document.body, install_collection.view(ctrl));
+});
